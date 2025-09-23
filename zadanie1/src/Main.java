8736bcc376ef0c67d12f02162cafede27144d637
Szymon Góral <matiqueue@gmail.com>
2025-09-23 11:57:14 +0200

    Zad1

--- zadanie1/src/Main.java
+++ zadanie1/src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Zad.1
+        double stopnieCelcjusza, stopnieFahrenheita;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę stopni Celsjuszach:");
+        stopnieCelcjusza = scanner.nextDouble();
+        //
+        //Fahrenheita = 1.8 * stopnieCelsjusza + 32.0
+        stopnieFahrenheita = 1.8 * stopnieCelcjusza + 32;
+        System.out.println("Stopnie Fahrenheita: " + stopnieFahrenheita);
     }
 }
+
